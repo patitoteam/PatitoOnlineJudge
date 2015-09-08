aeb6f3d6bce8ff0a7809ff2b8fd8512726e68589
SamR <sam@scarlet-gost>
2015-09-08 15:33:44 -0400

    Uniendo

--- resources/views/submit/submit.blade.php
+++ resources/views/submit/submit.blade.php
@@ -7,7 +7,7 @@
 
   <style type="text/css" media="screen">
     body {
-        overflow: hidden;
+      overflow: hidden;
     }
     #submit{
       position: absolute;
@@ -15,44 +15,44 @@
       left: 48%;
     }
     #editor {
-        /*margin: 0;*/
-        position: absolute;
-        top: 6%;
-        bottom: 0;
-        left: 25%;
-        right: 0;
-        width: 50%;
-        height:50%;
+      /*margin: 0;*/
+      position: absolute;
+      top: 6%;
+      bottom: 0;
+      left: 25%;
+      right: 0;
+      width: 50%;
+      height:50%;
     }
   </style>
 </head>
 <body>
-<!--form-->
-<div style="text-align: center;">
+  <!--form-->
+  <div style="text-align: center;">
     Problem 1000<br>
     Language  
-     <select>
+    <select>
       <option value="c++">c++</option>
       <option value="java">java</option>
       <option value="c">C</option>
       <option value="pascal">pascal</option>
-     </select>
-</div>  
-<form>
-  
-  <pre id="editor" >
-  </pre>
-  <div id="submit">
-  <input type="submit" value="Enviar"/>
-  </div>
-</form>   
-<script type="text/javascript" src="{!! asset('editor/js/ace.js') !!}"></script>
-<script>
+    </select>
+  </div>  
+  <form>
+    
+    <pre id="editor" >
+    </pre>
+    <div id="submit">
+      <input type="submit" value="Enviar"/>
+    </div>
+  </form>   
+  <script type="text/javascript" src="{!! asset('editor/js/ace.js') !!}"></script>
+  <script>
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/twilight");
     editor.getSession().setMode("ace/mode/c_cpp");
     
-</script>
+  </script>
 
 
 
